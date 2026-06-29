ff4103f258f85750e181b363fe14161f6c82ef16
pratik-baghel <pratikb2608@gmail.com>
2026-06-29 17:17:01 +0530

    Create Day_28/Q109_check_matrix_is_identity_matrix.c

---
+++ Day_28/Q109_check_matrix_is_identity_matrix.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+ int a[10][10];
+ int n,i,j;
+ int flag=1;
+
+ printf("Enter order : ");
+ scanf("%d",&n);
+
+ for(i=0;i<n;i++)
+ {
+   for(j=0;j<n;j++)
+   {
+      scanf("%d",&a[i][j]);
+   }
+ }
+
+ for(i=0;i<n;i++)
+ {
+   for(j=0;j<n;j++)
+   {
+      if(i==j)
+      {
+         if(a[i][j]!=1)
+            flag=0;
+      }
+      else
+      {
+         if(a[i][j]!=0)
+            flag=0;
+      }
+   }
+ }
+
+ if(flag==1)
+    printf("Identity Matrix");
+
+ else
+    printf("Not Identity Matrix");
+
+ return 0;
+}
