8e04157117acaf6d8d01f0f0a17251a71220f7e8
pratik-baghel <pratikb2608@gmail.com>
2026-06-30 11:19:52 +0530

    Create Q74_subtract_two_matrices.c

---
+++ Day_19/Q74_subtract_two_matrices.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int r, c, a[10][10], b[10][10], diff[10][10];
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &b[i][j]);
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            diff[i][j] = a[i][j] - b[i][j];
+
+    printf("Difference of matrices:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++)
+            printf("%d ", diff[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
