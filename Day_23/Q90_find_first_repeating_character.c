d9a8b05e149b725f1190363032fef1135216d293
pratik-baghel <pratikb2608@gmail.com>
2026-06-30 11:38:33 +0530

    Create Q90_find_first_repeating_character.c

---
+++ Day_23/Q90_find_first_repeating_character.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int count[256] = {0};
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    for (int i = 0; str[i] != '\0'; i++)
+        count[(int)str[i]]++;
+
+    int found = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (count[(int)str[i]] > 1) {
+            printf("First repeating character = %c\n", str[i]);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+        printf("No repeating character found\n");
+
+    return 0;
+}
